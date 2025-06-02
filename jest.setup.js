7eff4c7ae4e4d3b338a4af4c2313dce1b2b4b84a
Kosmas Rio Legowo <kosmasrio.legowo@gmail.com>
2025-06-02 23:43:37 +0700

    perbaiki testing2

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,9 @@
 // jest.setup.js
 import '@testing-library/jest-dom';
+import 'cross-fetch/polyfill';
+// atau jika ingin mock:
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([]),
+  })
+);
